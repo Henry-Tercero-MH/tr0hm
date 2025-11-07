ed7c14f0b57265e2bae788d22d56b0687a7c3065
Henry-Tecero-MH <hterceroh@miumg.edu.gt>
2025-11-07 17:35:04 -0600

    se cargo un nuevo diseño

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -221,6 +221,13 @@ function Header() {
                   </svg>
                   <span>Mensajes</span>
                 </Link>
+                <Link href="/requests" className="mobile-item" onClick={() => setShowMobileMenu(false)}>
+                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden>
+                    <path d="M18 8A6 6 0 1 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M13.73 21a2 2 0 0 1-3.46 0" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+                  </svg>
+                  <span>Notificaciones</span>
+                </Link>
                 <div className="mobile-divider" />
                 {!user && (
                   <Link href="/login" className="mobile-item mobile-cta" onClick={() => setShowMobileMenu(false)}>Login</Link>
